4e9efac2769d090d7fa10f76f75365f92fdf5b32
fomenkokipz23-debug <fomenko.k_ipz23@rcit.ukr.education>
2026-09-08 17:42:54 +0300

    Lab1: додано клас Book за варіантом 1

---
+++ lab1v1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace lab1v1;
+
+public class Book
+{
+    
+    private string title;
+    private string author;
+
+    
+    public int Year { get; set; }
+
+    
+    public Book(string title, string author, int year)
+    {
+        this.title = title;
+        this.author = author;
+        Year = year;
+    }
+
+    
+    ~Book()
+    {
+        Console.WriteLine($"Об'єкт книги '{title}' вилучено з пам'яті.");
+    }
+
+    
+    public string GetInfo()
+    {
+        return $"Книга: \"{title}\" | Автор: {author} | Рік видання: {Year}";
+    }
+}
